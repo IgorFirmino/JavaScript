fe903e4e5e7d64cb7fc03b555bdd32eb5174bcfa
Igor Dos Santos Firmino <105456978+IgorFirmino@users.noreply.github.com>
2022-06-09 15:14:45 -0300

    09/06

--- Modulo D/Exercícios/Exercício 01/interação.js
+++ Modulo D/Exercícios/Exercício 01/interação.js
@@ -1,6 +1,20 @@
-var agora = new Date()
-var hora = agora.getHours()
+function carregar() {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.getElementById('imagem')
+    var date = new Date()
+    //var horas = date.getHours()
+   // var horas = getMinutes()
+   var horas = 21
+    msg.innerHTML = `Agora são ${horas} horas.`
 
-if(hora <= 11){
-    
+    if(horas > 0 && horas < 12){
+        img.src = 'fotos/manhã.png'
+        document.body.style.background ='#e2cd9f'
+    }else if(horas >= 12 && horas <= 18){
+        img.src = 'fotos/tarde.png'
+        document.body.style.background ='#BAA29D'
+    }else {
+        img.src = 'fotos/noite.png'
+        document.body.style.background ='#103778'
+    }
 }
--- Modulo D/Exercícios/Exercício. 2/interação.js
+++ Modulo D/Exercícios/Exercício. 2/interação.js
@@ -1,52 +1,18 @@
 function verificar(){
-    var data=new Date()
-    var ano=data.getFullYear()
-    var fano=document.getElementById('txtano')
-    var res=document.getElementById('res')
-    
-    if(fano.value.length ==0 || fano.value > ano){
-        window.alert('ERRO!!! Verifique os dados e tente novamente!')}
-        else{var fsex=document.getElementsByName('radsex')
-        var idade=ano - Number(fano.value)
-        var gênero=''
-        var img = document.createElement('img')
-        img.setAttribute('id','foto')
+    var nasc = document.querySelector('input#txtano')
+    var masc = document.querySelector('input#mas')
+    var femi = document.querySelector('input#fem')
+    var res = document.querySelector('div#res')
+    var sex = document.getElementsByClassName('radsex')
+    var nas = Number(nasc)
+    var data = new Date
+    var ano = data.getFullYear()
+    var idade = ano - nas
 
-        if(fsex[0].checked){gênero='Homem'
-            if(idade>=00 && idade <=11){
-                //criança
-                img.setAttribute('src','criança-m.png')
-            }else if(idade<= 18){
-                //jovem
-                img.setAttribute('src','adolescente m.png')
-            }else if(idade < 64){
-                //adulto
-                img.setAttribute('src','adulto m.png')
-            }else if(idade <130){
-                //idoso
-                img.setAttribute('src','velho.png')
-            }
-            } else if (fsex[1].checked){gênero='Mulher'
-            if(idade>=00 && idade <=11){
-                //criança
-                img.setAttribute('src','criança f.png')
-            }else if(idade<= 18){
-                //jovem
-                img.setAttribute('src','adolescente f.png')
-            }else if(idade < 64){
-                //adulto
-                img.setAttribute('src','adulto-f.png')
-            }else if(idade <130){
-                //idoso 
-                img.setAttribute('src','velha f.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `<p>Detectamos ${gênero} com ${idade} anos.</p>`
-        img.style.textAlign='center'
-        res.appendChild(img)
+    if(sex == masc){
+        window.alert('asddasd')
+    }else if(sex == femi){
+
     }
-    
-    
-    
+
 }
